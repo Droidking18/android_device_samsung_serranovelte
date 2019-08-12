cc906bb4b74d5b1720a9e98903147869cb107511
Dovi KAPLAN <dkaplan@c5r10s3.wethinkcode.co.za>
2019-08-12 10:17:10 +0200

    add viperos makefiles

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,5 @@ LOCAL_PATH := device/samsung/serranovelte
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_serranovelte.mk \
 	$(LOCAL_DIR)/pa_serranovelte.mk \
+	$(LOCAL_DIR)/viper_serranovelte.mk \
 	$(LOCAL_DIR)/lineage_serranovelte.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 for i in eng userdebug user; do
 	add_lunch_combo lineage_serranovelte-${i}
+	add_lunch_combo viper_serranovelte-${i}
 done
