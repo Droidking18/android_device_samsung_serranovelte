ebff1f7cc0df95483df6c0d0554cc8f26c15e4cd
Dovi Kaplan <dkaplan@student.wethinkcode.co.za>
2019-08-13 00:02:33 +0200

    fix profman issues possibly

--- viper_serranovelte.mk
+++ viper_serranovelte.mk
@@ -16,6 +16,11 @@
 # Inherit from common
 $(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
+
+# This can prevent profman error while building boot image preferences.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+
 $(call inherit-product, device/samsung/serranovelte/device.mk)
 
 ## Device identifier. This must come after all inclusions
